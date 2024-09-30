b1f5728337a1126dddf02e3fd987a43c0fb3777a
Lucas-sgarioni <lsgarioni@ucs.br>
2024-09-29 21:23:12 -0300

    Reestruturação API

--- rest_api/views.py
+++ rest_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from base.models import Contato
+from reserva.models import Reserva
 
 @api_view(['POST'])
 def adicionar_categoria(request):
@@ -37,6 +38,33 @@ def verificar_categorias(request):
             'e-mail': categoria.email,
             'mensagem': categoria.mensagem,
             'data': categoria.data,
+        }
+        dados.append(dado)
+    return Response(dados)
+
+@api_view(['GET'])
+def verificar_reserva(request):
+    consulta = Reserva.objects.all()
+    dados = []
+    for itens in consulta:
+        dado = {
+            'Informações do dono:':{
+            'nome': itens.nomeDoTutor,
+            'email': itens.email,
+            'telefone': itens.telefone,
+            },
+
+            'Informações do PET':{
+                'nome': itens.nomeDoPet,
+                'raça': itens.raca,
+                'tamanho': itens.tamanho,
+            },
+            
+            'Informações sobre a reserva:':{
+                'data': itens.data,
+                'hora': itens.horario,
+            },
+            'observacao': itens.observacoes,
         }
         dados.append(dado)
     return Response(dados)
